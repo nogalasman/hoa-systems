1a3f1e5d0217b171662adf5b8d55257eb7414a1a
Nogalasman <noga.lasman@gmail.com>
2021-01-24 11:57:32 +0200

    add voting filter and add icon

--- src/pages/VotingsPage/VotingsPage.js
+++ src/pages/VotingsPage/VotingsPage.js
@@ -1,17 +1,35 @@
 import VotingsCard from '../../components/VotingsCard/VotingsCard';
+import SearchIcon from '@material-ui/icons/Search';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import './VotingsPage.css';
+import { TextField } from '@material-ui/core';
+import { useState } from 'react';
 
 function VotingsPage(props) {
     const { votings, usersNum } = props;
+    const [filter, setFilter] = useState("");
+
+    let notActiveVotings = votings.filter(function (voting) {
+        if (voting.isActiveVoting()){
+            return false;
+        }
+        const full = (voting.title + " " + voting.details).toLowerCase();
+        return full.includes(filter.toLowerCase());
+    });
 
     return(
         <div className="p-votings">
             <div className="content-item">
                 <h1>Active Votings</h1>
+                <div className="add-voting-container"><AddCircleOutlineIcon className="add-voting-img" /></div>
                 <VotingsCard className="content-item" votings={votings.filter(v => v.isActiveVoting())} usersNum={usersNum}></VotingsCard></div>
             <div className="content-item">
                 <h1>Voting Results</h1>
-                <VotingsCard className="content-item" votings={votings.filter(v => !v.isActiveVoting())} usersNum={usersNum}></VotingsCard></div>
+                <div className="filter-control">
+                    <SearchIcon className="search-icon" />
+                    <TextField className="form-control" label="filter by text in title or details" onChange={e => setFilter(e.target.value)} />
+                </div>
+                <VotingsCard className="content-item" votings={notActiveVotings} usersNum={usersNum}></VotingsCard></div>
         </div>
     );
 }
